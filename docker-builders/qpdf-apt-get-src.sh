985b176ebaaf457be9c56598ed80ea9d2f7144e5
Trenton H <holmes.trenton@gmail.com>
2022-09-19 13:36:54 -0700

    Use a case statement, it's easier

--- docker-builders/qpdf-apt-get-src.sh
+++ docker-builders/qpdf-apt-get-src.sh
@@ -2,19 +2,30 @@
 
 set -eux
 
-echo "Getting the correct source source"
+echo "Getting the correct source source & version"
 
-src_src="sid"
-deb_version="${QPDF_VERSION}-1"
-
-if [ "${QPDF_VERSION}" = "10.6.3" ] ; then
-	src_src="bookworm"
-	deb_version="${QPDF_VERSION}-1"
-fi;
+case "${QPDF_VERSION}" in
+	"10.6.3")
+		src_src="bookworm"
+		deb_version="${QPDF_VERSION}-1"
+		;;
+	"11.0.0")
+		src_src="sid"
+		deb_version="${QPDF_VERSION}-3"
+		;;
+	"11.1.0")
+		src_src="sid"
+		deb_version="${QPDF_VERSION}-1"
+		;;
+	*)
+		echo "No match for ${QPDF_VERSION}"
+		exit 1
+		;;
+esac
 
 echo "Choosing source from $src_src version $deb_version"
 
-echo "deb-src http://deb.debian.org/debian/ $src_src main" > /etc/apt/sources.list.d/$src_src-src.list
+echo "deb-src http://deb.debian.org/debian/ $src_src main" > "/etc/apt/sources.list.d/$src_src-src.list"
 
 apt-get update
 
